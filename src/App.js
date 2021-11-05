3d5bf6bb14de480e0b76bf2642d69353399fc85d
shareekaepps <shareekaepps@icloud.com>
2021-11-05 10:35:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
 import "./App.css";
+import babyYoda from "./images/cookieGrogu.jpeg";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      snackCount: 0,
+    };
+  }
+
+  giveSnack = () => {
+    this.setState({
+      snackCount: this.state.snackCount + 1,
+    });
+  };
+
   render() {
-    return <div className="App">Snack App goes here!</div>;
+    return (
+      <div className="App">
+        <h1>Give Baby Yoda some snacks.</h1>
+        <img src={babyYoda} alt="baby yoda" />
+        <div className="card">
+          <div>
+            Baby Yoda has eaten{" "}
+            <span className="snackCount">{this.state.snackCount}</span> snacks.
+          </div>
+
+          <button onClick={this.giveSnack}>Give snack</button>
+        </div>
+      </div>
+    );
   }
 }
 
